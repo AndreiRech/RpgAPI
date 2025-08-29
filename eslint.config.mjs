21a8389fafe8ce91d17c4606faf29d65071d0925
Andrei <andreirech22@outlook.com>
2025-08-29 18:56:33 -0300

    fix: ESLint configuration now working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,18 +28,10 @@ export default tseslint.config(
       "sonarjs": sonarjsPlugin,
     },
     rules: {
-     ...tseslint.configs.recommended.rules,
-     ...tseslint.configs.strict.rules,
-      
-     ...nestjsTypedPlugin.configs.recommended.rules,
-     ...sonarjsPlugin.configs.recommended.rules,
-
-      "@darraghor/nestjs-typed/api-methods-should-be-guarded": "error",
-      
       "@typescript-eslint/interface-name-prefix": "off",
       "@typescript-eslint/explicit-function-return-type": "error",
       "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-explicit-any": "error"
     },
   },
 
